9f09660e5a731ecc4fb5191f6401b1f3a6532c5d
unknown <dcm@cs.uns.edu.ar>
2022-09-12 10:30:06 -0300

    Clean more

--- routes.js
+++ routes.js
@@ -1,37 +1,13 @@
 const express = require('express')
-const Joi = require('@hapi/joi')
-const { insertItem, getItems, getPelis, getPelisT, updateQuantity } = require('./db')
+const { insertItem,  getPelis, getPelisT } = require('./db')
 
 const router = express.Router()
-
-const itemSchema = Joi.object().keys({
-  name: Joi.string(),
-  quantity: Joi.number().integer().min(0)
-})
 
 router.get('/public',(req,res) => {
   res.sendFile(__dirname + "/public");
 })
 
-router.post('/peliculas', (req, res) => {
-  const item = req.body
-  console.log(req.body)
-  const result = itemSchema.validate(item)
-  if (result.error) {
-    console.log(result.error)
-    res.status(400).end()
-    return
-  }
-  insertItem(item)
-    .then(() => {
-      res.status(200).end()
-    })
-    .catch((err) => {
-      console.log(err)
-      res.status(500).end()
-    })
-})
-
+// Obtener las peliculas solicitadas
 router.get('/peliculas', (req, res) => {
   getPelis()
     .then((items) => {
@@ -46,14 +22,19 @@ router.get('/peliculas', (req, res) => {
     })
 })
 
-router.get('/items/:titulo', (req, res) => {
-  const {titulo} = req.params
-  getPelisT(titulo)
-    .then((items) => {
-      items = items.map((item) => ({
-        title: item.title
-      }))
-      res.json(items)
+// Postear una pelicula
+router.post('/peliculas', (req, res) => {
+  const item = req.body
+  console.log(req.body)
+  const result = itemSchema.validate(item)
+  if (result.error) {
+    console.log(result.error)
+    res.status(400).end()
+    return
+  }
+  insertItem(item)
+    .then(() => {
+      res.status(200).end()
     })
     .catch((err) => {
       console.log(err)
